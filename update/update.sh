45205ffea16614225ac71547dae81c81ab586406
Mark Moog <markmoog@gmail.com>
2018-01-14 18:16:03 -0500

    Copywrite year updated with JavaScript

--- update/update.sh
+++ update/update.sh
@@ -1,7 +1,7 @@
 # !/bin/bash
 
 echo "Downloading latest game results"
-wget https://www.kenpom.com/cbbga18.txt
+wget https://kenpom.com/cbbga18.txt
 mv ./cbbga18.txt ./data/cbbga18.txt
 
 echo "Generaging edges for win tree"
@@ -17,9 +17,9 @@ echo "Updaing the rankings analysis"
 python3 ./scripts/ranking_comparison.py
 
 echo "Moving files into local website"
-mv edges2018.json ../trees/edges2018.json
-mv ratings_2018.csv ../ratings/ratings_2018.csv
-mv ranking_analysis.json ../vega/data/ranking_analysis.json
+mv edges2018.json ../data/win_trees/edges2018.json
+mv ratings_2018.csv ../data/ratings/ratings_2018.csv
+mv ranking_analysis.json ../data/ranking_analysis/ranking_analysis.json
 
 echo "Updating dates on local website"
 # currently assuming data is from the previous day (-1d), might want to add an option to specify the date
@@ -34,12 +34,12 @@ echo "Uploading local changes to AWS"
 aws s3 cp ../documents/ratings.html s3://www.markmoog.com/documents/ratings.html
 aws s3 cp ../documents/win_trees.html s3://www.markmoog.com/documents/win_trees.html
 aws s3 cp ../documents/ranking_analysis.html s3://www.markmoog.com/documents/ranking_analysis.html
-aws s3 cp ../ratings/ratings_2018.csv s3://www.markmoog.com/ratings/ratings_2018.csv
-aws s3 cp ../trees/edges2018.json s3://www.markmoog.com/trees/edges2018.json
-aws s3 cp ../vega/data/ranking_analysis.json s3://www.markmoog.com/vega/data/ranking_analysis.json
+aws s3 cp ../data/ratings/ratings_2018.csv s3://www.markmoog.com/data/ratings/ratings_2018.csv
+aws s3 cp ../data/win_trees/edges2018.json s3://www.markmoog.com/data/win_trees/edges2018.json
+aws s3 cp ../data/ranking_analysis/ranking_analysis.json s3://www.markmoog.com/data/ranking_analysis/ranking_analysis.json
 
 echo "Invalidating old files on CloudFront"
-aws cloudfront create-invalidation --distribution-id E2QBM02Z5J7Y5V --paths /documents/ratings.html /documents/win_trees.html /documents/ranking_analysis.html /trees/edges2018.json /ratings/ratings_2018.csv /vega/data/ranking_analysis.json
+aws cloudfront create-invalidation --distribution-id E2QBM02Z5J7Y5V --paths /documents/ratings.html /documents/win_trees.html /documents/ranking_analysis.html /data/trees/edges2018.json /data/ratings/ratings_2018.csv /data/ranking_analysis/ranking_analysis.json
 
 echo "Cleaning up"
 rm output.csv
